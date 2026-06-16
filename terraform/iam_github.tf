efde6bda4d04ad95577c7470e7e1a34b5b5fec32
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2026-06-16 15:14:07 -0300

    add GitHub permissions to get, put, and list objects in the Terraform state bucket

---
+++ terraform/iam_github.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_user_policy" "github_s3_backend_access" {
+  name = "s3-backend-access"
+  user = "github-lambda-deploy-pipeline" # ajuste pro nome real do seu usuário
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::lambda-deploy-pipeline-terraform-state",
+          "arn:aws:s3:::lambda-deploy-pipeline-terraform-state/*"
+        ]
+      }
+    ]
+  })
+}
