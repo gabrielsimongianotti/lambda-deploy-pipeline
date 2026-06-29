d82d7f5cda27f76224e88da9bf8e528c2ba23d39
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2026-06-29 19:34:44 -0300

    test: i forgot turn false destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,10 +67,28 @@
 #   retention_in_days = 14
 # }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+
+resource "aws_s3_bucket" "bucket-images" {
+  bucket        = var.bucket_name
+  force_destroy = true
+  tags = {
+    Name = "files"
+  }
 }
 
-resource "aws_s3_bucket" "test1" {
-  bucket = "test1"
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.bucket-images.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
