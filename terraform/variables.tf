6e14c9d8d4847b32ea1cea175489110a0c88414b
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2026-06-29 18:42:36 -0300

    fix: testing a new command

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+# variable "aws_region" {
+#   type    = string
+#   default = "us-east-1"
+# }
 
-variable "function_name" {
-  type = string
-}
+# # variable "function_name" {
+# #   type = string
+# # }
 
-variable "environment" {
-  type    = string
-  default = "staging"
-}
+# variable "environment" {
+#   type    = string
+#   default = "staging"
+# }
 
 variable "bucket_name" {
   type = string
